c06e567be6641e1b56de126596ef91797fab5062
laxmi2882007 <l3655648@gmail.com>
2026-09-25 16:07:33 +0530

    Fix indentation and logic in vowel check

--- vowels.py
+++ vowels.py
@@ -1,5 +1,5 @@
 string(input("Enter a string:")
-       if(a==i):
-          print("it is an vowel")
-       else:
-          print("it is not a vowel")
+if(a==i):
+   print("it is not a vowel")
+else:
+   print("it is an vowel")
